7ac7d3d7f81c919a5441722ab33b1084dee4fc79
anpalo <alockdall@gmail.com>
2025-10-12 17:00:35 +0900

    P: Add sidebar html and css

---
+++ frontend/sidebar.js
@@ -0,0 +1,12 @@
+fetch('/sidebar.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('sidebar-container').innerHTML = html;
+
+    const hamburger = document.getElementById('hamburger');
+    const sidebar = document.getElementById('sidebar');
+
+    if (hamburger && sidebar) {
+      hamburger.addEventListener('click', () => sidebar.classList.toggle('open'));
+    }
+  });
