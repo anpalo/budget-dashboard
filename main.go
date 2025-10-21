58df44e92e87d71538e29c9ba36a8df1cbad2b0a
anpalo <alockdall@gmail.com>
2025-10-21 11:38:55 +0900

    U2: Update main.go and remove unused parsing funcs and api calls for testing

--- main.go
+++ main.go
@@ -34,16 +34,6 @@ func main() {
 	budget.PrintMonthlyTotals(monthlyTotals)
 	budget.PrintHighestSpendingCategory(monthlyTotals)
 
-	yearlyCategoryTotals, yearlyTotal := budget.ComputeYearlyTotals(rows, headers)
-	budget.PrintYearlyTotals(yearlyCategoryTotals, yearlyTotal)
-
-	averages := budget.ComputeMonthlyAverages(rows, monthlyTotals)
-	budget.PrintAllMonthAverages(averages)
-
-	totalSavings := budget.GetTotalSavings(rows, headers)
-	fmt.Printf("Total Savings (KRW): %d원\n", int64(totalSavings))
-
-
 
 	rates, err := currencies.GetRates()
 	if err != nil {
@@ -52,41 +42,13 @@ func main() {
 	}
 	fmt.Println("Rates:", rates)
 
-	// searchString := "LULU"
-	// matches, err := stocks.SearchSymbol(searchString)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-
-	// if len(matches) == 0 {
-    // 	fmt.Println("No matching symbols found")
-    // 	return
-	// }
-
-	// chosenSymbol := matches[0].Symbol
-	// chosenName := matches[0].Name
-	// timeSeries, err := stocks.GetStocks(chosenSymbol)
-	// if err != nil {
-    // 	fmt.Println("Error fetching stock data:", err)
-    // 	return
-	// }
-
-	// latestPrice := ""
-    // for _, values := range timeSeries {
-    //     latestPrice = values["4. close"]
-    //     break 
-    // }
-
-    // fmt.Printf("%s (%s) | Latest Price: %s %s\n",
-    // chosenName, chosenSymbol, latestPrice, matches[0].Currency)
-
-
+	
     http.HandleFunc("/api/monthly-totals", api.MonthlyTotalsHandler(monthlyTotals))
-	http.HandleFunc("/api/total-savings", api.TotalSavingsHandler(rows, headers))
     http.HandleFunc("/api/currency", api.CurrencyConversionHandler())
-	http.HandleFunc("/api/stocks", api.StocksHandler())
-	http.HandleFunc("/api/symbol-search", api.SymbolSearchHandler())
+
+	// http.HandleFunc("/api/total-savings", api.TotalSavingsHandler(rows, headers))
+	// http.HandleFunc("/api/stocks", api.StocksHandler())
+	// http.HandleFunc("/api/symbol-search", api.SymbolSearchHandler())
 
 
 fs := http.FileServer(http.Dir("./frontend"))
