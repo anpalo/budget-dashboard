32ec8438c9ddcc4a1b3ba600b7b2df69aff03b8f
anpalo <alockdall@gmail.com>
2025-11-16 18:30:37 +0900

    W: Clean up repetitive code | Clean file structure | Add update page

---
+++ frontend/add/add.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("add-row-button")
+const dateInput = document.getElementById("expense_date")
+const categoryInput = document.getElementById("category")
+const amountInput = document.getElementById("amount")
+const commentInput = document.getElementById("comment");
